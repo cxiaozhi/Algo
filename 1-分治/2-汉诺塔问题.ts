235d6532fb45f6eed986041fe3012f6dc30d39d2
ccz <2047638475@qq.com>
2024-12-09 11:00:43 +0800

    feat: 🎸 完成汉诺塔问题

--- 1-分治/2-汉诺塔问题.ts
+++ 1-分治/2-汉诺塔问题.ts
@@ -4,3 +4,29 @@
 2.每次只能移动一个圆盘。
 3.小圆盘必须时刻位于大圆盘之上。
 */
+function move(src: number[], target: number[]): void {
+    const pan = src.pop();
+    target.push(pan);
+}
+let srcG = [6,5,4,3,2,1];
+let bufG = [];
+let targetG = [];
+//  求解汉诺塔问题
+function dfs2(i: number, src: number[], buf: number[], target: number[]): void {
+    if (i === 1) {
+        move(src, target);
+        return;
+    }
+    dfs2(i - 1, src, target, buf);
+    move(src, target);
+    console.log(srcG, bufG, targetG);
+    dfs2(i - 1, buf, src, target);
+}
+
+function solveHanota(A: number[], B: number[], C: number[]): void {
+    const n = A.length;
+    dfs2(n, A, B, C);
+}
+solveHanota(srcG, bufG, targetG);
+console.log(srcG, bufG, targetG);
+
