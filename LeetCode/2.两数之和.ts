db318eabd42fa727bb3c60dbe0560c5a911be56b
ccz <2047638475@qq.com>
2024-12-17 12:00:39 +0800

    feat: 🎸 完成两数之和

--- LeetCode/2.两数之和.ts
+++ LeetCode/2.两数之和.ts
@@ -1,3 +1,24 @@
 /**
- * 力扣：https://leetcode.cn/problems/two-sum/description/
+ * 力扣:https://leetcode.cn/problems/two-sum/description/
  */
+class TwoSum {
+    nums = [2, 7, 11, 15];
+    target = 19;
+    twoSum(nums: number[], target: number): number[] {
+        let temp = [];
+        for (let index = 0; index < nums.length - 1; index++) {
+            for (let j = index + 1; j < nums.length; j++) {
+                if (nums[index] +  nums[j] == target) {
+                    temp.push(index)
+                    temp.push(j);
+                    return temp;
+                };
+            }
+        }
+        return temp;
+    }
+}
+
+let twoSum = new TwoSum();
+
+console.log(twoSum.twoSum([3, 2, 3], 6));
