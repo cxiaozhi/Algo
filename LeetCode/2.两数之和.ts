7beb2992caecdbd35c3879bc8e6e3c6bc4e1563b
ccz <2047638475@qq.com>
2024-12-18 18:43:22 +0800

    feat: 🎸 完成两数之和优化

--- LeetCode/2.两数之和.ts
+++ LeetCode/2.两数之和.ts
@@ -5,17 +5,28 @@ class TwoSum {
     nums = [2, 7, 11, 15];
     target = 19;
     twoSum(nums: number[], target: number): number[] {
-        let temp = [];
-        for (let index = 0; index < nums.length - 1; index++) {
-            for (let j = index + 1; j < nums.length; j++) {
-                if (nums[index] +  nums[j] == target) {
-                    temp.push(index)
-                    temp.push(j);
-                    return temp;
-                };
+        // for (let index = 0; index < nums.length - 1; index++) {
+        //     for (let j = index + 1; j < nums.length; j++) {
+        //         if (nums[index] +  nums[j] == target) {
+        //             temp.push(index)
+        //             temp.push(j);
+        //             return temp;
+        //         };
+        //     }
+        // }
+        // while(nums[right] > target){
+        //     right--
+        // }
+        let hash = {};
+        for (let i = 0; i < nums.length; i++) {
+            console.log(hash);
+            
+            if (hash[target - nums[i]] !== undefined) {
+                return [hash[target - nums[i]], i];
             }
+            hash[nums[i]] = i;
         }
-        return temp;
+        return [];
     }
 }
 
