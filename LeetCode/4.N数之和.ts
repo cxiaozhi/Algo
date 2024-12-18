f71c46bb47803b85d23b4bab9899cd1d7e52f30b
Ccz <2047638475@qq.com>
2024-12-18 22:56:43 +0800

    test: 💍 未完成

--- LeetCode/4.N数之和.ts
+++ LeetCode/4.N数之和.ts
@@ -1,3 +1,10 @@
 /**
  * 力扣：https://github.com/sisterAn/JavaScript-Algorithms/issues/128
+ * 请用算法实现，从给定的无序、不重复的数组A中，取出N个数，使其相加和为M。并给出算法的时间、空间复杂度
  */
+var arr = [1, 4, 7, 11, 9, 8, 10, 6];
+var N = 3;
+var M = 27;
+function nNumSum(nums,n,m) {
+    
+}
