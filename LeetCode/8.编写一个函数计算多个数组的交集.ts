4e7b1cb12d1251c469c099430510c8326f6aac30
Ccz <2047638475@qq.com>
2025-01-04 21:14:50 +0800

    feat: 🎸 完成算法8

--- LeetCode/8.编写一个函数计算多个数组的交集.ts
+++ LeetCode/8.编写一个函数计算多个数组的交集.ts
@@ -1,3 +1,13 @@
 /**
  * 力扣：https://github.com/sisterAn/JavaScript-Algorithms/issues/10
  */
+const getIntersetiton = (...arrs)=>{
+    return Array.from(new Set(arrs.reduce((total,arr)=>{
+        console.log(total,arr);
+        
+        return arr.filter(item=>total.includes(item))
+    })))
+}
+
+
+console.log(getIntersetiton([1,2,3],[2,3,4]));
