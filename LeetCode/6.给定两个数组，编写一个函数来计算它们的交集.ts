80088cdfb121a5413fcf0cc1b4ca13ef83ba9b3f
Ccz <2047638475@qq.com>
2025-01-04 16:27:03 +0800

    feat: 🎸 完成6

--- LeetCode/6.给定两个数组，编写一个函数来计算它们的交集.ts
+++ LeetCode/6.给定两个数组，编写一个函数来计算它们的交集.ts
@@ -1,3 +1,11 @@
 /**
  * 力扣：https://github.com/sisterAn/JavaScript-Algorithms/issues/6
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2] 输出: [2]
  */
+namespace s6 {
+    let nums1 = [1, 2, 2, 1],
+        nums2 = [2, 2];
+
+    const fn = (n1, n2) => [...new Set(n1.filter((i) => n2.includes(i)))];
+    console.log(fn(nums1, nums2));
+}
