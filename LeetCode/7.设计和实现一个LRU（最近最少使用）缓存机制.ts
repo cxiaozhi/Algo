28df87d65fab1040da381362735c30b19e47e2e9
Ccz <2047638475@qq.com>
2025-01-04 18:18:12 +0800

    feat: 🎸 完成算法7

--- LeetCode/7.设计和实现一个LRU（最近最少使用）缓存机制.ts
+++ LeetCode/7.设计和实现一个LRU（最近最少使用）缓存机制.ts
@@ -1,3 +1,26 @@
 /**
  * 力扣：https://github.com/sisterAn/JavaScript-Algorithms/issues/7
  */
+class LRU {
+    private cache: Map<string, string> = new Map();
+    max:number = 100
+    get(k: string) {
+        if (this.cache.has(k)) {
+            return this.cache.get(k);
+        }
+        return -1;
+    }
+    put(k: string, v: string) {
+        if (this.cache.size >= this.max) {
+            const key = this.cache.keys().next().value
+            this.cache.delete(key) // 删除第一个
+        }
+        this.cache.set(k,v)
+    }
+}
+
+const lru = new LRU()
+
+lru.put("1","-----")
+console.log(lru.get("1"));
+
