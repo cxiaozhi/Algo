0ea5cb661338ec2022f0f377f6a3e16ebd59801c
ccz <2047638475@qq.com>
2025-01-08 14:12:01 +0800

    feat: 🎸 完成算法10

--- LeetCode/10.判断一个单链表是否有环.ts
+++ LeetCode/10.判断一个单链表是否有环.ts
@@ -1,3 +1,22 @@
+import { ListNode } from "./9.合并两个有序链表"
+
 /**
  * 力扣：https://leetcode.cn/problems/linked-list-cycle/description/
+ * 思路：双指针，有一个快指针 和 慢指针，慢指针每次走一步，快指针每次走两步，如有环，快慢指针会 相遇
  */
+function hasCycle(head: ListNode | null): boolean {
+    let set = new Set()
+    while (head) {
+        if (set.has(head)) {
+            return true
+        }
+        set.add(head)
+        head = head.next
+    }
+    return false
+};
+let set = new Set()
+let nodepp = new ListNode()
+set.add(nodepp)
+console.log(set.has(nodepp));
+
