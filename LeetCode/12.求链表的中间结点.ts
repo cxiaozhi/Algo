b2e70263516b1b2c3c5f0d671879d7c4d2bccdec
ccz <2047638475@qq.com>
2025-01-09 20:53:10 +0800

    feat: 🎸 完成algo12

--- LeetCode/12.求链表的中间结点.ts
+++ LeetCode/12.求链表的中间结点.ts
@@ -1,3 +1,13 @@
+import { ListNode } from "./9.合并两个有序链表";
+
 /**
  * 力扣：https://leetcode.cn/problems/middle-of-the-linked-list/description/
  */
+function middleNode(head: ListNode | null): ListNode | null {
+    let arr = []
+    while (head) {
+        arr.push(head)
+        head = head.next
+    }
+    return arr[Math.floor(arr.length/2)]
+};
