ee7dd00f9a7d28b464ae2fa5c439c8fc037772b2
ccz <2047638475@qq.com>
2025-01-10 19:05:30 +0800

    feat: 🎸 完成算法14

--- LeetCode/14.编写一个程序，找到两个单链表相交的起始节点.ts
+++ LeetCode/14.编写一个程序，找到两个单链表相交的起始节点.ts
@@ -1,3 +1,27 @@
+import { ListNode } from "./9.合并两个有序链表";
+
 /**
  * 力扣：https://leetcode.cn/problems/intersection-of-two-linked-lists/description/
  */
+function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+    let temp1 = []
+    let temp2 = []
+    while (headA) {
+        temp1.push(headA)
+        headA = headA.next
+    }
+
+    while (headB) {
+        temp2.push(headB)
+        headB = headB.next
+    }
+    
+    for (let index = 0; index < temp1.length; index++) {
+        const item = temp1[index];
+        if (temp2.includes(item)) {
+            return item
+        }
+        
+    }
+    return null
+};
