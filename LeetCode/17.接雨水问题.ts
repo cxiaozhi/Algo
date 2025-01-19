ed687b28f29e2a05eb6310ce0f2f18fcdffd6a26
Ccz <2047638475@qq.com>
2025-01-19 21:24:06 +0800

    feat: 🎸 完成al17

--- LeetCode/17.接雨水问题.ts
+++ LeetCode/17.接雨水问题.ts
@@ -1,3 +1,32 @@
 /**
  * 力扣：https://leetcode.cn/problems/trapping-rain-water/description/
+ * 思路：双指针，那边小就移动哪边的指针
  */
+function trap(height: number[]): number {
+    let count = 0
+    let left = 0
+    let right = height.length - 1
+    let leftH = 0
+    let rightH = 0
+    while ( right > left) {
+        if (leftH > height[left]) {
+            count = leftH - height[left] + count
+        }else{
+            leftH = height[left]
+        }
+        if (rightH > height[right]) {
+            count = rightH - height[right] + count
+        }else{
+            rightH = height[right]
+        }
+        if (leftH < rightH) {
+            left++;
+        }else {
+            right--;
+        }
+    }
+    return count
+};
+
+
+console.log(trap([2,0,2]));
