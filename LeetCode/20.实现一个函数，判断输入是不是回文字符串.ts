3eaafa9c48362f73cfc4dbf61c3330e24dbcc6dc
Ccz <2047638475@qq.com>
2025-01-19 22:26:22 +0800

    feat: 🎸 完成al20

--- LeetCode/20.实现一个函数，判断输入是不是回文字符串.ts
+++ LeetCode/20.实现一个函数，判断输入是不是回文字符串.ts
@@ -1,3 +1,13 @@
 /**
  * 力扣：https://github.com/sisterAn/JavaScript-Algorithms/issues/20
  */
+function IsPalindrome(str: string) {
+    if (str == null || str.length < 1) return false;
+    let left = 0,right = str.length - 1;
+    while (left < right) {
+        if (str.charAt(left) !== str.charAt(right)) return false
+        ++left;
+        --right;
+    }
+    return true
+}
