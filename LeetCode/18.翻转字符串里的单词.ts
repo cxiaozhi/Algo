d60078ccf75bf802c3dbb2ca85b731e9f5308227
Ccz <2047638475@qq.com>
2025-01-19 21:53:32 +0800

    feat: 🎸 完成al18

--- LeetCode/18.翻转字符串里的单词.ts
+++ LeetCode/18.翻转字符串里的单词.ts
@@ -1,3 +1,9 @@
 /**
  * 力扣：https://leetcode.cn/problems/reverse-words-in-a-string/description/
  */
+
+function reverseWords(s: string): string {
+    return s.trim().split(" ").reverse().filter((s) => s.trim()).join(" ")
+}
+
+console.log(reverseWords("a good   example"));
