f2ea8a8b8eabb652a405b7b201a96934f6f98db5
Ccz <2047638475@qq.com>
2025-01-19 22:20:10 +0800

    feat: 🎸 完成al19

--- LeetCode/19.最长公共前缀（LCP）.ts
+++ LeetCode/19.最长公共前缀（LCP）.ts
@@ -1,3 +1,13 @@
 /**
  * 力扣：https://leetcode.cn/problems/longest-common-prefix/description/
  */
+function longestCommonPrefix(strs: string[]): string {
+    let i = 0;
+    const minL = Math.min(...strs.map((str) => str.length));
+    for (i = 0; i < minL; i++) {
+        if (strs.some((str) => str[i] !== strs[0][i])) return strs[0].substring(0, i);
+    }
+    return strs[0].substring(0, minL);
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
