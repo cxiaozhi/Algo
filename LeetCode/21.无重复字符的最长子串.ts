6c8990272927bb0e43c7d0ce9faffb00de92d455
Ccz <2047638475@qq.com>
2025-01-31 12:56:47 +0800

    feat: 🎸 完成algo21

--- LeetCode/21.无重复字符的最长子串.ts
+++ LeetCode/21.无重复字符的最长子串.ts
@@ -1,3 +1,22 @@
 /**
+ * 求解：无重复字符的最长子串;
  * 力扣：https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/
+ * 链表技巧：递归、虚拟节点、双指针
+ * 思路：滑动窗口
  */
+function lengthOfLongestSubstring(s: string): number {
+    let maxLength = 0;
+    let left = 0;
+    const charSet = new Set(); // 窗口
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++;
+        }
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);// 每一次滑动都会比较一次最大长度
+    }
+    return maxLength
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
