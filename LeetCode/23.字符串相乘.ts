b2d7e2d6921ac9a31c0bc8d5ef32ff59e01f7911
Ccz <2047638475@qq.com>
2025-01-31 19:57:49 +0800

    feat: 🎸 完成algo23

--- LeetCode/23.字符串相乘.ts
+++ LeetCode/23.字符串相乘.ts
@@ -6,7 +6,7 @@ function multiply(num1: string, num2: string): string {
     if (Number(num1) == 0 || Number(num2) == 0) return "0";
     const m = num1.length,
         n = num2.length;
-    const ans = new Array(m + n).fill(0);
+    const ans = new Array(m + n).fill("0");
     for (let i = m - 1; i >= 0; i--) {
         for (let j = n - 1; j >= 0; j--) {
             const multi = +num1[i] * +num2[j];
@@ -17,7 +17,7 @@ function multiply(num1: string, num2: string): string {
             ans[p1] += Math.floor(sum / 10);
         }
     }
-    while(ans[0] == "0"){
+    while(ans[0] === "0"){
         ans.shift()
     }
     return ans.join("")
