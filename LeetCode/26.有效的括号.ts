4f8021f25324cb2789f4b355c6daf1162b13f353
Ccz <2047638475@qq.com>
2025-02-04 17:51:59 +0800

    feat: 🎸 完成algo26

--- LeetCode/26.有效的括号.ts
+++ LeetCode/26.有效的括号.ts
@@ -1,3 +1,31 @@
 /**
  * 力扣：https://leetcode.cn/problems/valid-parentheses/description/
  */
+function isValid(s: string): boolean {
+    let arr = [];
+    for (let index = 0; index < s.length; index++) {
+        const element = s[index];
+        if (element == "(" || element == "{" || element == "[") {
+            arr.push(element);
+        } else if (element == ")") {
+            let str = arr.pop();
+            if (str !== "(") {
+                return false;
+            }
+        } else if (element == "}") {
+            let str = arr.pop();
+            if (str !== "{") {
+                return false;
+            }
+        } else if (element == "]") {
+            let str = arr.pop();
+            if (str !== "[") {
+                return false;
+            }
+        }
+    }
+    if (arr.length > 0) return false;
+    return true;
+}
+
+console.log(isValid("[(])"));
