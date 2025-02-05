3303488e23315a00c4785160273b033597ce181c
ccz <2047638475@qq.com>
2025-02-05 14:28:49 +0800

    feat: 🎸 完成algo41

--- LeetCode/40.二叉树的层序遍历.ts
+++ LeetCode/40.二叉树的层序遍历.ts
@@ -1,3 +1,19 @@
 /**
  * 力扣：https://github.com/sisterAn/JavaScript-Algorithms/issues/47
+ * 和41题重复
  */
+function levelOrder(root: TreeNode | null): number[][] {
+    let result = []
+    let que = [root]
+    while (que.length) {
+        let node = que.shift()
+        result.push(node.val)
+        if (node.left) {
+            que.push(node.left)
+        }
+        if (node.right) {
+            que.push(node.right)
+        }
+    }
+    return result
+};
--- LeetCode/41.二叉树的层次遍历.ts
+++ LeetCode/41.二叉树的层次遍历.ts
@@ -1,3 +1,25 @@
 /**
  * 力扣: https://leetcode.cn/problems/binary-tree-level-order-traversal-ii/description/
  */
+function levelOrderBottom(root: TreeNode | null): number[][] {
+    if (!root) return [];
+    let result = [];
+    let que = [root];
+    while (que.length) {
+        let curr = [],
+            temp = [];
+        while (que.length) {
+            let node = que.shift();
+            curr.push(node.val);
+            if (node.left) {
+                temp.push(node.left);
+            }
+            if (node.right) {
+                temp.push(node.right);
+            }
+        }
+        result.push(curr);
+        que = temp;
+    }
+    return result.reverse();
+}
