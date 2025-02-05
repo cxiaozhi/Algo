48c98fe09a3fbd0e4207702d30dfca199637dd2e
ccz <2047638475@qq.com>
2025-02-05 10:49:43 +0800

    feat: 🎸 完成algo36

--- LeetCode/36.第一个只出现一次的字符.ts
+++ LeetCode/36.第一个只出现一次的字符.ts
@@ -1,3 +1,11 @@
 /**
  * 力扣：https://leetcode.cn/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/description/
  */
+function dismantlingAction(arr: string): string {
+    let map = new Map();
+    for (const char of arr) map.set(char, (map.get(char) ?? 0) + 1);
+    for (const [key, val] of map) if (val == 1) return key;
+    return " ";
+}
+
+console.log(dismantlingAction("loveleetcode"));
