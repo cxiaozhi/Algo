46c2ae8ebf8f05a5d6bcbb8431b48af4276cc0d0
ccz <2047638475@qq.com>
2025-02-06 17:56:40 +0800

    feat: 🎸 完成algo45

--- LeetCode/45.平衡二叉树.ts
+++ LeetCode/45.平衡二叉树.ts
@@ -1,3 +1,20 @@
 /**
- * 力扣: https://leetcode.cn/problems/binary-tree-inorder-traversal/description/
+ * 力扣: https://leetcode.cn/problems/balanced-binary-tree/description/
  */
+
+import { TreeNode } from "../1-分治/1-构建树问题";
+
+// 自底向上
+function isBalanced(root: TreeNode | null): boolean {
+    function balanced(node: TreeNode) {
+        if (!node) return 0;
+        const left = balanced(node.left)
+        const right = balanced(node.right)
+        if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+            return -1 // 返回是否是平衡子树
+        }
+        return Math.max(left,right) + 1 // 返回深度
+    }
+
+    return balanced(root) !== -1
+}
