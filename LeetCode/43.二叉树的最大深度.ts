81f734521123c669fd2988b7c604077403f8ec95
ccz <2047638475@qq.com>
2025-02-06 15:34:29 +0800

    feat: 🎸 完成algo43

--- LeetCode/43.二叉树的最大深度.ts
+++ LeetCode/43.二叉树的最大深度.ts
@@ -1,3 +1,7 @@
 /**
  * 力扣: https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
  */
+function maxDepth(root: TreeNode | null): number {
+    if (!root) return 0;
+    return 1 + Math.max(maxDepth(root.left),maxDepth(root.right))
+}
