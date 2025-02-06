d8caf4827cb8593a3c83e66e9b01cd69a156777f
ccz <2047638475@qq.com>
2025-02-06 18:29:03 +0800

    feat: 🎸 完成algo46

--- LeetCode/46.路径总和.ts
+++ LeetCode/46.路径总和.ts
@@ -1,3 +1,18 @@
+import { TreeNode } from "../1-分治/1-构建树问题";
+
 /**
  * 力扣: https://leetcode.cn/problems/path-sum/description/
  */
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+    if (!root) return false;
+    function dfs(root: TreeNode | null, count: number) {
+        if (!root) return false;
+
+        count += root.val;
+        if (!root.left && !root.right) {
+            return count == targetSum;
+        }
+        return dfs(root.left, count) || dfs(root.right, count);
+    }
+    return dfs(root, 0);
+}
