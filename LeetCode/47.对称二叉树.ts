d1651efd62996ff37133c605e850d62d9ed9e5d5
ccz <2047638475@qq.com>
2025-02-06 20:13:57 +0800

    feat: 🎸 完成algo47

--- LeetCode/47.对称二叉树.ts
+++ LeetCode/47.对称二叉树.ts
@@ -1,3 +1,21 @@
+import { TreeNode } from "../1-分治/1-构建树问题";
+
 /**
  * 力扣: https://leetcode.cn/problems/symmetric-tree/description/
  */
+function isSymmetric(root: TreeNode | null): boolean {
+    function dfs(left: TreeNode,right: TreeNode) {
+        if (!left && !right) {
+            return true
+        }
+        if (!left || !right) {
+            return false
+        }
+        if (left.val !== right.val) {
+            return false
+        }
+
+        return dfs(left.left,right.right) && dfs(left.right,right.left)
+    }
+    return dfs(root.left,root.right)
+}
