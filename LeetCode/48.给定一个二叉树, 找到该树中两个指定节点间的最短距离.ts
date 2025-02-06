f9d88c00a7bd00efebb61586cb924aad0c405951
ccz <2047638475@qq.com>
2025-02-06 21:08:27 +0800

    feat: 🎸 完成algo49

--- LeetCode/48.给定一个二叉树, 找到该树中两个指定节点间的最短距离.ts
+++ LeetCode/48.给定一个二叉树, 找到该树中两个指定节点间的最短距离.ts
@@ -1,3 +1,4 @@
 /**
  * 力扣: https://github.com/sisterAn/JavaScript-Algorithms/issues/82
+ * 字节一面 先找最近公共祖先 再求祖先到两个节点间的距离
  */
--- LeetCode/49.二叉搜索树中第K小的元素.ts
+++ LeetCode/49.二叉搜索树中第K小的元素.ts
@@ -1,3 +1,22 @@
+import { TreeNode } from "../1-分治/1-构建树问题";
+
 /**
  * 力扣: https://leetcode.cn/problems/kth-smallest-element-in-a-bst/description/
  */
+function kthSmallest(root: TreeNode | null, k: number): number {
+    let count = 0;
+    let tart = 0;
+    function dfs(root: TreeNode | null) {
+        if (!root) return;
+        dfs(root.left);
+        dfs(root.right);
+        count++;
+        console.log(count);
+        if (count == k) {
+            tart = root.val;
+            return;
+        }
+    }
+    dfs(root);
+    return tart;
+}
