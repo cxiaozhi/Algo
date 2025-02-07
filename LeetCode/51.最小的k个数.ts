62996a4829e55d28dfd67e28e7def663af869a92
ccz <2047638475@qq.com>
2025-02-07 15:10:57 +0800

    feat: 🎸 完成algo51

--- LeetCode/51.最小的k个数.ts
+++ LeetCode/51.最小的k个数.ts
@@ -1,3 +1,7 @@
 /**
  * 力扣: https://leetcode.cn/problems/zui-xiao-de-kge-shu-lcof/description/
  */
+function inventoryManagement(stock: number[], cnt: number): number[] {
+    stock.sort((a, b) => a - b); // 这题的提升效率的关键是这个排序算法的实现
+    return stock.slice(0, cnt);
+}
