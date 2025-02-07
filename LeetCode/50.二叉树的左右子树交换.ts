987b322f8fb623ab8085c2063b07fd889966b11f
ccz <2047638475@qq.com>
2025-02-07 10:39:33 +0800

    feat: 🎸 完成algo50
    ✅ Closes: g

--- LeetCode/50.二叉树的左右子树交换.ts
+++ LeetCode/50.二叉树的左右子树交换.ts
@@ -1,3 +1,14 @@
+import { TreeNode } from "../1-分治/1-构建树问题";
+
 /**
  * 力扣: https://leetcode.cn/problems/invert-binary-tree/description/
  */
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if (!root) return root;
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
+}
