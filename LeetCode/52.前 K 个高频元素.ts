3bf2255e02b09e51eb946512c9034b2d21b3c3cf
ccz <2047638475@qq.com>
2025-02-07 15:26:56 +0800

    feat: 🎸 完成algo52

--- LeetCode/52.前 K 个高频元素.ts
+++ LeetCode/52.前 K 个高频元素.ts
@@ -1,3 +1,16 @@
 /**
  * 力扣: https://leetcode.cn/problems/top-k-frequent-elements/description/
  */
+function topKFrequent(nums: number[], k: number): number[] {
+    let map = new Map();
+    nums.map((num) => map.set(num, (map.get(num) ?? 0) + 1));
+    let arr = Array.from(map);
+    arr.sort((a, b) => b[1] - a[1]);
+    let temp = [];
+    arr.map((val, index) => {
+        if (index < k) {
+            temp.push(val[0]);
+        }
+    });
+    return temp;
+}
