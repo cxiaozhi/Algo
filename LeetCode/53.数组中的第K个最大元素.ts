74ca9095ac77dd54491952bd58aebb75c951c6ef
ccz <2047638475@qq.com>
2025-02-07 15:50:45 +0800

    feat: 🎸 完成algo53

--- LeetCode/53.数组中的第K个最大元素.ts
+++ LeetCode/53.数组中的第K个最大元素.ts
@@ -1,3 +1,7 @@
 /**
  * 力扣: https://leetcode.cn/problems/kth-largest-element-in-an-array/description/
  */
+function findKthLargest(nums: number[], k: number): number {
+    nums.sort((a, b) => b - a);
+    return nums[k - 1];
+}
