d23eea41b6d3daa0d7868050232ccfdd13f32fce
ccz <2047638475@qq.com>
2025-02-08 17:47:14 +0800

    feat: 🎸 完成algo64
    ✅ Closes: git push

--- LeetCode/64.扑克牌问题.ts
+++ LeetCode/64.扑克牌问题.ts
@@ -1,3 +1,18 @@
 /**
  * 力扣: https://github.com/sisterAn/JavaScript-Algorithms/issues/80
  */
+const runBackward = () => {
+    const res = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1] ;
+    const base = [];
+    for (let i = 0; i < res.length; i++) {
+        if (base.length) {
+            const item = base.splice(base.length - 1, 1)[0];
+            base.unshift(item);
+        }
+        base.unshift(res[i])
+    }
+    return base;
+}
+
+console.log(runBackward());
+
