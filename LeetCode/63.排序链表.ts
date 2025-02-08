677aa27669ebe7da0c8beeaa6fc873a85b78461b
ccz <2047638475@qq.com>
2025-02-08 17:21:55 +0800

    feat: 🎸 完成algo63

--- LeetCode/63.排序链表.ts
+++ LeetCode/63.排序链表.ts
@@ -1,3 +1,25 @@
+import { ListNode } from "./9.合并两个有序链表";
+
 /**
  * 力扣: https://leetcode.cn/problems/sort-list/description/
  */
+function sortList(head: ListNode | null): ListNode | null {
+    if (!head) return head;
+    let arr = [];
+    let start = head;
+    while (start) {
+        arr.push([start.val, start]);
+        start = start.next;
+    }
+    arr.sort((a, b) => a[0] - b[0]);
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        const next = arr[index + 1];
+        if (next) {
+            element[1].next = next[1];
+        } else {
+            element[1].next = null;
+        }
+    }
+    return arr[0][1];
+}
