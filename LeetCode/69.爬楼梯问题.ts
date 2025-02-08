03b61634d808f49485d76830310aaaab01b8d829
ccz <2047638475@qq.com>
2025-02-08 19:35:15 +0800

    feat: 🎸 完成algo69

--- LeetCode/69.爬楼梯问题.ts
+++ LeetCode/69.爬楼梯问题.ts
@@ -1,3 +1,18 @@
 /**
  * 力扣: https://leetcode.cn/problems/climbing-stairs/description/
  */
+function climbStairs(n: number): number {
+    if (n == 1) {
+        return 1;
+    }
+    let first = 1;
+    let second = 2;
+    for (let index = 3; index < n + 1; index++) {
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+    return second;
+}
+
+console.log(climbStairs(60));
