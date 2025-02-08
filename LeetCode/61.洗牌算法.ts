6e028bf7c92c90b491f8f856c4aedb52bf07ca9e
ccz <2047638475@qq.com>
2025-02-08 16:59:31 +0800

    feat: 🎸 完成algo61

--- LeetCode/61.洗牌算法.ts
+++ LeetCode/61.洗牌算法.ts
@@ -1,3 +1,26 @@
 /**
  * 力扣: https://leetcode.cn/problems/shuffle-an-array/description/
  */
+class Solution {
+    private nums: number[] = [];
+    constructor(nums: number[]) {
+        this.nums = nums;
+    }
+    reset(): number[] {
+        return this.nums;
+    }
+
+    shuffle(): number[] {
+        let copy = [...this.nums];
+        for (let index = copy.length - 1; index > 0; index--) {
+            const random = Math.floor(Math.random() * (index + 1));
+            [copy[index], copy[random]] = [copy[random], copy[index]];
+        }
+        return copy;
+    }
+}
+
+let obj4 = new Solution([1, 2, 3]);
+let arr11 = obj4.reset();
+let arr12 = obj4.shuffle();
+console.log(arr12);
