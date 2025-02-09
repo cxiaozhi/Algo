0a980482b42b7b49601d0044b3a4d2188152c3d2
Ccz <2047638475@qq.com>
2025-02-10 00:49:22 +0800

    feat: 🎸 完成algo80

--- LeetCode/80.括号生成.ts
+++ LeetCode/80.括号生成.ts
@@ -1,3 +1,28 @@
 /**
  * 力扣: https://leetcode.cn/problems/generate-parentheses/description/
  */
+function generateParenthesis(n: number): string[] {
+    const result: string[] = [];
+    const path: string[] = [];
+    function back(left: number, right: number) {
+        if (path.length === 2 * n) {
+            result.push(path.join(""));
+            return;
+        }
+        
+        if (left < n) {
+            path.push("(");
+            back(left + 1, right);
+            path.pop();
+        }
+        if (right < left) {
+            path.push(")");
+            back(left, right + 1);
+            path.pop();
+        }
+    }
+    back(0, 0);
+    return result;
+}
+console.log(generateParenthesis(3));
+
