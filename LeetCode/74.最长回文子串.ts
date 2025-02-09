ab7b231df6bdc7d8162ac285be60ca4cde249f31
Ccz <2047638475@qq.com>
2025-02-09 19:49:16 +0800

    feat: 🎸 完成algo74
    ]

--- LeetCode/74.最长回文子串.ts
+++ LeetCode/74.最长回文子串.ts
@@ -1,3 +1,27 @@
 /**
  * 力扣: https://leetcode.cn/problems/longest-palindromic-substring/description/
  */
+function longestPalindrome(s: string): string {
+    if (s.length < 2) return s;
+    let start = 0;
+    let maxLen = 1;
+    function eAC(left: number, right: number) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        const len1 = eAC(i, i);
+        const len2 = eAC(i, i + 1);
+        const len = Math.max(len1, len2);
+        if (len > maxLen) {
+            maxLen = len;
+            start = i - Math.floor((len - 1) / 2);
+        }
+    }
+    return s.slice(start, start + maxLen);
+}
+console.log(longestPalindrome("cbbd"));
