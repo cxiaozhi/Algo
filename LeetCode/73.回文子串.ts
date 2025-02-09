fc89a600e8b1c7bb69bfdccd8deb264c9d126432
Ccz <2047638475@qq.com>
2025-02-09 19:29:22 +0800

    feat: 🎸 完成algo73

--- LeetCode/73.回文子串.ts
+++ LeetCode/73.回文子串.ts
@@ -1,3 +1,35 @@
 /**
  * 力扣: https://leetcode.cn/problems/palindromic-substrings/description/
  */
+function countSubstrings(s: string): number {
+    let win = 1;
+    let arr = [];
+    function check(str: string) {
+        let left = 0;
+        let right = str.length - 1;
+        while (right > left) {
+            if (str[left] === str[right]) {
+                left++;
+                right--;
+            } else return false;
+        }
+        return true;
+    }
+    while (win <= s.length) {
+        let left = 0,
+            right = win - 1;
+        for (let index = 0; index < s.length; index++) {
+            const element = s.slice(left, right + 1);
+            if (check(element)) {
+                arr.push(element);
+            }
+            left++;
+            right++;
+            if (right==s.length) break
+        }
+        win++;
+    }
+    return arr.length;
+}
+
+console.log(countSubstrings("aaa"));
