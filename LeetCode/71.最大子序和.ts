681c7af7add3ad0e020d7b06ec8818d0ba63cee3
Ccz <2047638475@qq.com>
2025-02-09 18:46:17 +0800

    feat: 🎸 完成algo71

--- LeetCode/71.最大子序和.ts
+++ LeetCode/71.最大子序和.ts
@@ -1,3 +1,17 @@
 /**
  * 力扣: https://leetcode.cn/problems/maximum-subarray/description/
  */
+
+function maxSubArray(nums: number[]): number {
+    let maxSum = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        maxSum = Math.max(sum, maxSum);
+        console.log(maxSum);
+        if (sum < 0) sum = 0;
+    }
+
+    return maxSum;
+}
+console.log(maxSubArray([-2, -1, -3, -4, -1, -2, -1, -5, -4]));
