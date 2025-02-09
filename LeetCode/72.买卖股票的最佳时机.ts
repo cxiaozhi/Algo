9e3bcfe31b1c118fb48e73b156afb36ccc5c1768
Ccz <2047638475@qq.com>
2025-02-09 19:05:35 +0800

    feat: 🎸 完成algo72

--- LeetCode/72.买卖股票的最佳时机.ts
+++ LeetCode/72.买卖股票的最佳时机.ts
@@ -1,3 +1,16 @@
 /**
  * 力扣: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/description/
  */
+function maxProfit(prices: number[]): number {
+    let max = 0;
+    let minP = Infinity;
+    for (let index = 0; index < prices.length; index++) {
+        const element = prices[index];
+        if (element > minP) {
+            max = Math.max(max, element - minP);
+        }
+        minP = Math.min(element, minP);
+    }
+    return max;
+}
+console.log(maxProfit([7, 6, 4, 3, 1]));
