daa909323e94304930d00e36b882c7515d18dbf0
Ccz <2047638475@qq.com>
2025-02-10 00:33:21 +0800

    feat: 🎸 完成algo79

--- LeetCode/79.全排列问题.ts
+++ LeetCode/79.全排列问题.ts
@@ -1,3 +1,26 @@
 /**
  * 力扣: https://leetcode.cn/problems/permutations/description/
  */
+function permute(nums: number[]): number[][] {
+    const result: number[][] = [];
+    const path: number[] = [];
+    const used: boolean[] = new Array(nums.length).fill(false);
+    function backtrack() {
+        if (path.length === nums.length) {
+            result.push([...path]);
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            path.push(nums[i]);
+            used[i] = true;
+            backtrack();
+            path.pop();
+            used[i] = false;
+        }
+    }
+    backtrack();
+    return result;
+}
+console.log(permute([1,2,3]));
+
